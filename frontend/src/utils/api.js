59e5bef5b1fea6af4c2bcef214b04feb8e4b5f9f
Bitodette <farriiedd@gmail.com>
2026-05-28 21:01:42 +0700

    feat: update api url to render singapore

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000';
+export const API_BASE = import.meta.env.VITE_API_BASE || 'https://kelolain-be-kgf4.onrender.com';
 
 export const api = axios.create({
-  baseURL: API_BASE,
+  baseURL: `${API_BASE}`,
 });
+
